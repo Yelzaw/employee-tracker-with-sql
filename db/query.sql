79a045f937e7fba72b44b15176d96d0e1d0bc118
Yelzaw <110345251+Yelzaw@users.noreply.github.com>
2023-01-27 23:26:23 -0400

    added queries

--- db/query.sql
+++ db/query.sql
@@ -1,5 +1,33 @@
-SELECT movies.movie_name AS movie, reviews.review
-FROM reviews
-LEFT JOIN movies
-ON reviews.movie_id = movies.id
-ORDER BY movies.movie_name;
+-- Query to view all employee
+SELECT e.id, e.first_name, e.last_name, title, name, salary, CONCAT(m.first_name,' ',m.last_name) as manager
+FROM employee as e 
+LEFT JOIN role as r
+ON e.role_id = r.id
+LEFT JOIN department as d 
+ON r.department_id = d.id
+LEFT JOIN employee as m 
+ON m.id = e.manager_id;
+
+-- Query to view all departments
+SELECT id, name FROM department;
+
+-- Query to view all roles
+SELECT role.id, role.title, department.name as department, role.salary
+FROM role
+LEFT JOIN department
+ON role.department_id = department.id;
+
+-- Query to add department
+INSERT INTO department (name)
+VALUES ("Service");
+
+-- Query to add role
+INSERT INTO role (title, salary, department_id)
+VALUES ("Customer Service", 80000, 5);
+
+-- Query to add employee
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Sam", "Kash", 9, 3)
+
+-- Query to update Employee Role
+UPDATE employee SET role_id = 1 WHERE id = 9;
--- db/schema.sql
+++ db/schema.sql
@@ -3,19 +3,7 @@ CREATE DATABASE employees_db;
 
 USE employees_db;
 
-CREATE TABLE employee(
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INT,
-  manager_id INT,
-  FOREIGN KEY (role_id)
-  REFERENCES role(id)
-  ON DELETE SET NULL,
-  FOREIGN KEY (manager_id)
-  REFERENCES employee(id)
-  ON DELETE SET NULL
-);
+
 
 CREATE TABLE department(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -29,5 +17,19 @@ CREATE TABLE role(
   department_id INT,
   FOREIGN KEY (department_id)
   REFERENCES department(id)
+  ON DELETE SET NULL
+);
+
+CREATE TABLE employee(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY (role_id)
+  REFERENCES role(id)
+  ON DELETE SET NULL,
+  FOREIGN KEY (manager_id)
+  REFERENCES employee(id)
   ON DELETE SET NULL
 );
