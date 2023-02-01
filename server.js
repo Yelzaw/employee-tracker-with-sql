3855ab61841ae784b0c0f833ecdfde2be12bc2bd
Yelzaw <110345251+Yelzaw@users.noreply.github.com>
2023-02-01 13:10:42 -0400

    added comments

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('events').EventEmitter.defaultMaxListeners = 0;
 const express = require('express');
 const Employees = require('./lib/employees')
 const employees = new Employees();
+
 // Import and require mysql2
 const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
@@ -11,18 +12,6 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Connect to database
-// const db = mysql.createConnection(
-//   {
-//     host: 'localhost',
-//     // MySQL username,
-//     user: 'root',
-//     // TODO: Add MySQL password here
-//     password: 'kawthaung',
-//     database: 'employees_db'
-//   },
-//   console.log(`Connected to the employees_db database.`)
-// );
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
